e429776deded0aa7ad89c6fe74db34478fc1670d
Zephruz <kyleb9816@gmail.com>
2018-10-25 13:38:53 -0500

    Updated string.random to use os.time() and SysTime()'s decimal value

--- lua/zlib/includes/stringrandom.lua
+++ lua/zlib/includes/stringrandom.lua
@@ -10,9 +10,9 @@ for i = 65,  90 do table.insert(charset, string.char(i)) end
 for i = 97, 122 do table.insert(charset, string.char(i)) end
 
 function string.random(length)
-	math.randomseed(os.time())
-
 	if length < 0 then return "" end
+	
+	math.randomseed(os.time() + (SysTime()%1)^5)
 	
 	return string.random(length - 1) .. charset[math.random(1, #charset)]
 end
